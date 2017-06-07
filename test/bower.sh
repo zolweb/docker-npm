a7b243501230c2bdd915b9cc752502beec4e21ad
Michael Kenney <mkenney@webbedlam.com>
2017-06-06 20:53:00 -0600

    fix bower test (#117)

--- test/bower.sh
+++ test/bower.sh
@@ -4,7 +4,7 @@ PREFIX="        "
 
 CMD="$PROJECT_PATH/bin/bower"
 if [ "" != "$1" ]; then
-    CMD="docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$1 /run-as-user /usr/local/bin/bower"
+    CMD="docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$1 /run-as-user /usr/local/bin/bower  --allow-root"
 fi
 
 cd $PROJECT_PATH/test/resources
